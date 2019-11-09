3c8dd1d4c94f1642bfd4b4e54ae45b0898cc6b71
sephirothrr <sephirothrr@gmail.com>
2019-11-08 19:25:47 -0500

    nesting fixes

--- pace-underrep/pace_underrep/settings.py
+++ pace-underrep/pace_underrep/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8fa766a9-c3e9-4dd3-8c88-2f28c615c647'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8fa766a9-c3e9-4dd3-8c88-2f28c615c647')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
