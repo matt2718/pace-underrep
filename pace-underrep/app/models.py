f2d9e243eb5985f75b07ab2d35e8dad607333542
sephirothrr <sephirothrr@gmail.com>
2019-11-15 16:22:52 -0500

    syncing model work

--- pace-underrep/app/models.py
+++ pace-underrep/app/models.py
@@ -3,6 +3,15 @@ Definition of models.
 """
 
 from django.db import models
+from enum import Enum, auto
+import os
+from django.conf import settings
+
+CHOICE_DIR = os.path.join(settings.BASE_DIR, 'app/choices/')
+
+with open(os.path.join(CHOICE_DIR, 'gender.txt')) as f:
+        genders = f.read().splitlines()
+Gender = Enum('Gender', genders)
 
 # Create your models here.
 class Person(models.Model):
@@ -10,8 +19,7 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     age = models.IntegerField();
-
-
+    #gender = models.
 
 
 
